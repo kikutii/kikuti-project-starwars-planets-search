78d8edbf1d1a4f172dce5ebf7dce0a20d498474e
kikutii <joaovictorkikuti@gmail.com>
2022-07-27 08:17:37 -0300

    adicionado novos estados dentro do contexto e adicionando loading enquanto o filteredData estiver em requisição

--- src/context/StarWarPlanetsContext.js
+++ src/context/StarWarPlanetsContext.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import useData from '../hooks/useData';
+import useFilters from '../hooks/useFilters';
 import Loading from '../components/Loading';
 
 export const StarWarPlanetsContext = React.createContext();
 
 export function StarWarPlanetsProvider({ children }) {
   // esse data retorna os planets, a trybe exige que o nome da variável seja "data"
-  const data = useData();
+  const [data, filteredData, setFilteredData] = useData();
+  const [filters, setFilters] = useFilters();
 
   const context = {
     data,
+    filteredData,
+    filters,
+    setFilters,
+    setFilteredData,
   };
 
   return (
     <StarWarPlanetsContext.Provider value={ context }>
       {
-        data.length ? children : <Loading />
+        filteredData.length ? children : <Loading />
       }
     </StarWarPlanetsContext.Provider>
   );
