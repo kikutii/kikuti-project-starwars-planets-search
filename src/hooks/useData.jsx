177ae791f65052d5f73fa6290bca66024be54f50
kikutii <joaovictorkikuti@gmail.com>
2022-07-27 08:16:55 -0300

    apenas manda o valor do estado data para o filteredData, para deixar o data como valor inicial

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -3,13 +3,17 @@ import fetchPlanets from '../services/fetchPlanets';
 
 export default function useData() {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
+    if (data.length) {
+      return setFilteredData(data);
+    }
     const fetchData = async () => {
       setData(await fetchPlanets());
     };
     fetchData();
-  }, []);
+  }, [data]);
 
-  return data;
+  return [data, filteredData, setFilteredData];
 }
