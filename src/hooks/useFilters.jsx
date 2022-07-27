67160466b0b58c39f28802821e25548eb09a4ab9
kikutii <joaovictorkikuti@gmail.com>
2022-07-27 10:32:41 -0300

    adicionado filterByNumericValues no hook costumizável que vai salvar um histórico de filtros

--- src/hooks/useFilters.jsx
+++ src/hooks/useFilters.jsx
@@ -3,6 +3,13 @@ import { useState } from 'react';
 export default function useFilters() {
   const [filters, setFilters] = useState({
     filterByName: '',
+    filterByNumericValues: [
+      {
+        column: '',
+        comparison: '',
+        value: '',
+      },
+    ],
   });
 
   return [filters, setFilters];
