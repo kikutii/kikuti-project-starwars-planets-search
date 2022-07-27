37c0f942532591137e667b951e166e291928b071
kikutii <joaovictorkikuti@gmail.com>
2022-07-27 08:14:07 -0300

    adicionado componente Filters no App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Filters from './components/Filters';
 import Table from './components/Table';
 import { StarWarPlanetsProvider } from './context/StarWarPlanetsContext';
 
 function App() {
   return (
     <StarWarPlanetsProvider>
+      <Filters />
       <Table />
     </StarWarPlanetsProvider>
   );
