b74c28872401b4b4b84a3f64703091ba1e2a6fdc
kikutii <joaovictorkikuti@gmail.com>
2022-07-30 14:16:55 -0300

    feat: add order object initial value in hook useFilters

--- src/hooks/useFilters.jsx
+++ src/hooks/useFilters.jsx
@@ -4,6 +4,10 @@ export default function useFilters() {
   const [filters, setFilters] = useState({
     filterByName: '',
     filterByNumericValues: [],
+    order: {
+      order: 'population',
+      sort: 'ASC',
+    },
   });
 
   return [filters, setFilters];
